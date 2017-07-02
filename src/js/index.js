4d627a44f335981d996388137133783d6ee1747e
Ilya Gribov <imilyagribov@gmail.com>
2017-07-02 10:32:41 +0300

    news block and about block

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 
-$('.js-main-slider').slick({
-  arrows: true,
-  prevArrow: '.main-banner-slider__prev',
-  nextArrow: '.main-banner-slider__next',
-  autoplay: true
+jQuery(function($){
+
+    $('.js-main-slider').slick({
+      arrows: true,
+      prevArrow: '.main-banner-slider__prev',
+      nextArrow: '.main-banner-slider__next',
+      autoplay: true
+    });
+
+    $(document).on('click', '.job-activities-catalog__item--with-children > a', function() {
+        $(this).parent().toggleClass('active');
+    });
+
 });
